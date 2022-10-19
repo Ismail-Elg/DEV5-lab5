ab972b18edf315c630f3fd4a9ebef98c034284c9
Ismail <elghamri.ismail@gmail.com>
2022-10-19 15:31:08 +0200

    feat(Messages): made connection to local database with all routes

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -1,84 +1,111 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const messageSchema = new Schema({
-  user: String,
-  text: String,
-  completed: Boolean
-  });
+const Message = require("../../../models/Message");
 
-const Message = mongoose.model('Message', messageSchema);
-
-
-router.get('/', function(req, res, next) {
-  Message.find(function(err, messages) {
-    if (err) return console.error(err);
-    res.json(messages);
-  });
-});
- 
-
-router.get('/:id', function(req, res, next) {
-  res.json({
-      "status": "success",
-      "message": "GETTING message with id: " + req.params.id,
-      "data": {
-          "message": {
-              "user": "John",
-              "text": "Hello World"
-          }
+router.get("/", function (req, res, next) {
+  if (req.query.user) {
+    Message.find({ user: req.query.user }, function (err, messages) {
+      if (err) {
+        return next(err);
       }
+      if (messages.length === 0) {
+        return res.status(404).json({
+          status: "error",
+          message: "no messages found", 
+        });
+      }
+      res.json(messages);
+    });
+  } else {
+    Message.find(function (err, messages) {
+      if (err) {
+        return next(err);
+      }
+      res.json(messages);
+    });
+  }
+});
+
+router.get("/:id", function (req, res, next) {
+  if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+    res.status(404).json({
+      status: "error",
+      message: "Invalid ID",
+    });
+  }
+  Message.findById(req.params.id, function (err, message) {
+    if (err) return console.error(err);
+    res.json(message);
   });
 });
 
-
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
   let message = new Message({
     user: req.body.user,
     text: req.body.text,
-    completed: false
   });
   message.save((err, doc) => {
     if (err) {
       res.json({
         status: "error",
         message: "Error saving message",
-        data: null
       });
     } else {
       res.json({
         status: "success",
         message: "Message saved",
-        data: doc
+        data: doc,
       });
     }
   });
 });
 
-
-router.put('/:id', function(req, res, next) {
-  if(!req.body.user || !req.body.text){
-    res.json({
-      "status": "error",
-      "message": "UPDATING message failed",
+router.put("/:id", function (req, res, next) {
+  //check if id is valid
+  if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+    res.status(404).json({
+      status: "error",
+      message: "Invalid ID",
     });
   }
-  else{
-  res.json({
-    "status": "success",
-    "message": "UPDATING message with id: " + req.params.id,
+  Message.findById(req.params.id, function (err, message) {
+    if (err) return console.error(err);
+    message.user = req.body.user;
+    message.text = req.body.text;
+    message.save((err, doc) => {
+      if (err) {
+        res.json({
+          status: "error",
+          message: "Error updating message",
+        });
+      } else {
+        res.json({
+          status: "success",
+          message: "Message updated",
+          data: doc,
+        });
+      }
+    });
   });
-}
-
 });
 
-router.delete('/:id', function(req, res, next) {
-  res.json({
-    "status": "success",
-    "message": "DELETING message with id: " + req.params.id,
+router.delete("/:id", function (req, res, next) {
+  if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+    res.status(404).json({
+      status: "error",
+      message: "Invalid ID",
+    });
+  }
+  Message.findByIdAndRemove(req.params.id, function (err, message) {
+    if (err) return console.error(err);
+    res.json({
+      status: "success",
+      message: "Message deleted",
+      data: message,
+    });
   });
 });
 
+//check if ?user=name is in the query
 
 module.exports = router;
